181409aaad6de2d988c354c37a516dce1d2105b4
liaohua1 <1045725140@qq.com>
2020-02-10 12:50:09 +0800

    first commit

---
+++ src/main/java/com/macro/mall/tiny/MallTinyApplication.java
@@ -0,0 +1,15 @@
+package com.macro.mall.tiny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author liaohua1
+ * @date 2020/2/10 12:02
+ */
+@SpringBootApplication
+public class MallTinyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MallTinyApplication.class, args);
+    }
+}
